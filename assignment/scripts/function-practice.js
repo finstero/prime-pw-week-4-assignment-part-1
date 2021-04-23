e6b5757d953dbfa3176b3313d2933d33ffd25b5b
Olivia Finster <olivia.finster@gmail.com>
2021-04-23 17:57:41 -0500

    completed assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -75,14 +75,14 @@ console.log('should say undefined:', getLast(dogs));
 console.log('--- Question 7 ---')
 
 function findOf( value, array ){
-  for (item of array){
+  for (let item of array){
     if (item == value){
       return true;
     }
   }
   return false;
 }
-
+//leaving in extra practice functions for myself.
 function find( value, array ){
   for (let i=0; i<array.length; i++){
     if (array[i] == value){
@@ -167,18 +167,80 @@ console.log('which will be uppercase?', uppercase);
 // 9. Function to return the sum of all numbers in an array
 console.log('--- Question 9 ---');
 
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
+function sumAll( numArray ) {
+  let sum = 0 // initiates sum as 0
+  for (let num of numArray){ // loops through every item in parameter array
+    sum = num + sum; //loops through array, adding up numbers
+    console.log('the sum is:', sum, 'and the num is:', num);
+  }
   return sum;
 }
+let funNum = [2, 5, 11, 17];
+console.log(' the sum should be 35:', sumAll(funNum));
+
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+console.log('--- Question 10 ---');
 
-
+function onlyPositiveArray ( numArray) {
+  let posArray = [];
+  for (let num of numArray){ //loops through every item in array
+    if (num > 0){
+      posArray.push(num); // adds positive numbers in initial array into a new array
+    }
+  }
+  return posArray;
+}
+let oneArray = [1, -1, 2, -2, 3, 0];
+let negArray = [-5, -33, 0, -187];
+console.log('should be same array:', onlyPositiveArray (funNum));
+console.log('should be 1, 2, 3:', onlyPositiveArray (oneArray));
+console.log('should be empty:', onlyPositiveArray (negArray));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
+console.log('--- Question 11 ---');
+
+// Given a total due and an array representing the amount of change in your pocket, determine
+// whether or not you are able to pay for the item. Change will always be represented in the
+//following order: quarters, dimes, nickels, pennies.
+//To illustrate: changeEnough([25, 20, 5, 0], 4.25) should yield true, since having 25
+//quarters, 20 dimes, 5 nickels and 0 pennies gives you 6.25 + 2 + .25 + 0 = 8.50.
+// see https://edabit.com/challenge/erFxBbqzZPSegMwnc for full description
+
+function changeEnough(array, cost){
+  let change = 0;
+  if (array.length != 4){
+    console.log('error! please make sure your array has 4 items');
+  }
+  else {
+    change = array[0]*.25 + array[1]*.1 + array[2]*.05 + array[3]*.01;
+    if (change >= cost){
+      console.log('you have, $' + change, ', enough to buy the item!');
+    }
+    else {
+      console.log('oh no! you don\'t have enough money!');
+    }
+  }
+}
+
+let change1 = [25, 20, 5, 0];
+let cost = 4.25
+let change2 = [1, 5, 20, 33];
+let changeError = [1, 2];
+
+
+console.log('should be enough');
+changeEnough(change1, cost);
+
+console.log('should be not enough!')
+changeEnough(change2, cost);
+
+console.log('should be error message')
+changeEnough(changeError, cost);
+
+
+//lines
